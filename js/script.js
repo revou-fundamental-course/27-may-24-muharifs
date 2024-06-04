703283e59cab0d37cc65bddf7becb62b93d02026
personal-branding <muh.arifs1505@gmail.com>
2024-06-04 16:53:18 +0700

    js menu, section contact, footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 //Ini JavaScript File
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// klik menu
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik mouse di luar sidebar
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
